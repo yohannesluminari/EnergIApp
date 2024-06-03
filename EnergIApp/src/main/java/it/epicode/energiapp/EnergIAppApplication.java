09c5808fcc9274d7bf9d1c3e25c0418f55651467
yohannesluminari <y.luminari@gmail.com>
2024-06-03 11:14:31 +0200

    First commit

---
+++ EnergIApp/src/main/java/it/epicode/energiapp/EnergIAppApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.energiapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergIAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnergIAppApplication.class, args);
+	}
+
+}
---
+++ EnergIApp/src/test/java/it/epicode/energiapp/EnergIAppApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.energiapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergIAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
